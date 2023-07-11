fe0a3a992d4890868c376f209284fcbf0d725030
leonardomartinez <leonardo.lmv@gmail.com>
2023-07-11 00:54:35 -0600

    Adding reursivity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Main init");
+
+        Recursivity recursivity = new Recursivity();
+        System.out.println("El resultado de recursiva para 4 ... "+ recursivity.sum(4));
     }
 }
---
+++ src/Recursivity.java
@@ -0,0 +1,9 @@
+public class Recursivity {
+
+    int sum(int n){
+        if(n <= 0){
+            return 0;
+        }
+        return n + sum(n-1);
+    }
+}
